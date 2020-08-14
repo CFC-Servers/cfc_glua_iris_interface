305a849ea6dbc43f817029cd3655274ca2e8e710
PLally <11771225+PLally@users.noreply.github.com>
2020-08-14 04:39:05 -0400

    update iris with ulib roles

---
+++ lua/autorun/server/iris_init.lua
@@ -0,0 +1,42 @@
+local baseURL = ""
+
+local function postJson( endpoint, data, callbackSuccess, callbackFailure )
+    HTTP{
+        failed = callbackFailure,
+        success = callbackSuccess,
+        method = "POST",
+        url = baseURL .. endpoint,
+        body = util.TableToJSON( data )	,
+        type = "application/json"
+    }
+end
+
+local function logError( err )
+    print( "CFC Iris Interface HTTP error: " .. err)
+end
+
+local function sendGroupsToIris()
+	local userData = {}
+
+	for k, v in pairs( ULib.ucl.users ) do
+		userData[util.SteamIDTo64(k)] = v
+	end
+
+    postJson( "/ranks/bulk_update", {
+        users = userData,
+        realm = "cfc3",
+    }, nil, logError )
+end
+
+
+hook.Add( "ULibUserGroupChange", "CFC_IrisInterface_UpdateRanks", function( steamid, allows, denies, newGroup, oldGroup )
+    local steamid64 = util.SteamIDTo64( steamid )
+    postJson( "/ranks/bulk_update", {
+        users = {
+            [steamid64] = {
+                group = newGroup
+            }
+        },
+        realm = "cfc3"
+    } )
+end )
